488d85641d12b6af779a0445ca99e9dbaf478ddf
Tamires <tamirespatrocinioo@gmail.com>
2021-08-25 15:22:03 -0300

    update

--- model/Operacao.php
+++ model/Operacao.php
@@ -17,7 +17,7 @@ class Operacao
     function createDicastb($campo_2, $campo_3, $campo_4, $campo_5, $campo_6, $campo_7, $campo_8, $campo_9, $campo_10)
     {
 
-        $stmt = $this->con->prepare("insert into dicastb values(?,?,?,?,?,?,?,?,?)");
+        $stmt = $this->con->prepare("insert into dicastb values(?,?,?,?,?,?,?,?)");
         $stmt->bind_param("s,s,s,s,s,s,s,s,s", $campo_2, $campo_3, $campo_4, $campo_5, $campo_6, $campo_7, $campo_8, $campo_9, $campo_10);
 
         if ($stmt->execute())
@@ -29,7 +29,7 @@ class Operacao
     {
         $stmt = $this->con->prepare("Select * from dicastb");
         $stmt->execute();
-        $stmt->bind_result($uid, $regiao, $estado, $cidade, $imgLogo, $imgPoTur01, $imgPoTur02, $imgGast, $pontosturisticos, $gastronomilocal);
+        $stmt->bind_result($uid, $regiao, $estado, $cidade, $imgLogo, $imgPoTur01, $imgGast, $pontosturisticos, $gastronomilocal, $endereco);
 
         $dicas = array();
 
@@ -43,10 +43,10 @@ class Operacao
             $dica['cidade'] = $cidade;
             $dica['imgLogo'] = $imgLogo;
             $dica['imgPoTur01'] = $imgPoTur01;
-            $dica['imgPoTur02'] = $imgPoTur02;
             $dica['imgGast'] = $imgGast;
             $dica['pontosturisticos'] = $pontosturisticos;
             $dica['gastronomialocal'] = $gastronomilocal;
+            $dica['endereco'] = $endereco;
 
             array_push($dicas, $dica);
         }
@@ -56,7 +56,7 @@ class Operacao
     function updateDicastb($campo_1, $campo_2, $campo_3, $campo_4, $campo_5, $campo_6, $campo_7, $campo_8, $campo_9, $campo_10)
     {
 
-        $stmt = $this->con->prepare("update dicastb set regiao=?, estado=?, cidade=?, imgLogo=?, imgPoTur01=?, imgPoTur02=?, imgGast=?, pontosturisticos=?, gastronomialocal=? where uid=?");
+        $stmt = $this->con->prepare("update dicastb set regiao=?, estado=?, cidade=?, imgLogo=?, imgPoTur01=?, imgGast=?, pontosturisticos=?, gastronomialocal=?, endereco=? where uid=?");
 
         $stmt->bind_param("s,s,s,s,s,s,s,s,s,i", $campo_2, $campo_3, $campo_4, $campo_5, $campo_6, $campo_7, $campo_8, $campo_9, $campo_10, $campo_1);
 
